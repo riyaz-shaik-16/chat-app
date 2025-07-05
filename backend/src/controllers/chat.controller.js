6062ff71ee9eba5482cfca1ed951db1571b5a8f6
Riyaz <sr308379@gmail.com>
2025-07-05 21:27:33 +0530

    fix: correct import casing for asyncHandler utility

--- backend/src/controllers/chat.controller.js
+++ backend/src/controllers/chat.controller.js
@@ -1,6 +1,6 @@
 import Conversation from "../models/conversation.model.js";
 import User from "../models/user.model.js";
-import asyncHandler from "../utils/asyncHandler.js";
+import asyncHandler from "../utils/AsyncHandler.js";
 import ApiResponse from "../utils/ApiResponse.js"
 
 export const getSidebarUsers = asyncHandler(async (req, res) => {
