e8af4ef971c8f654ed6b4afc8f60489d10bd3ad9
Riyaz <sr308379@gmail.com>
2025-07-26 23:01:29 +0530

    feat: add root endpoint to confirm mail service is running

--- backend/mail/src/index.js
+++ backend/mail/src/index.js
@@ -8,6 +8,10 @@ startSendOtpConsumer();
 
 const app = express();
 
+app.get("/",(req,res)=>{
+  res.send("Mail service running!");
+})
+
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
